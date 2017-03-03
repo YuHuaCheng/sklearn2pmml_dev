9d065985e42cffefb004db88e0a17b6e4604d30d
Yu Hua Cheng <ycheng@YChengMacbookPro.local>
2017-03-03 13:16:46 -0500

    setup.py bug fix

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name = "sklearn2pmml",
-    version = __version__,
+    name = "sklearn2pmml",    
     description = "Python library for converting Scikit-Learn models to PMML",
     author = "Villu Ruusmann",
     author_email = "villu.ruusmann@gmail.com",
-    license = __license__,
     packages = [
         "sklearn2pmml",
         "sklearn2pmml.resources"
@@ -14,7 +12,7 @@ setup(
     package_data = {
         "sklearn2pmml.resources" : ["*.jar"]
     },
-    requires = [
+    install_requires = [
         "scikit-learn>=0.16.0",
         "sklearn-pandas>=0.0.10"
     ]
